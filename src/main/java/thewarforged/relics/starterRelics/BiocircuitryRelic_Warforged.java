bc8fff07a1feb6b056be07282877fe225437da9e
Josh Thor <JoshuaThornton.SoftwareDev@Gmail.com>
2025-04-02 22:20:45 -0400

    Creating Biocircuitry relic
    I created it. Doesn't do much.

---
+++ src/main/java/thewarforged/relics/starterRelics/BiocircuitryRelic_Warforged.java
@@ -0,0 +1,16 @@
+package thewarforged.relics.starterRelics;
+
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+
+import static thewarforged.TheWarforgedMod.makeID;
+
+public class BiocircuitryRelic_Warforged {
+    private static final String NAME = BiocircuitryRelic_Warforged.class.getSimpleName();
+    public static final String ID = makeID(NAME);
+    private static final AbstractRelic.RelicTier RARITY = AbstractRelic.RelicTier.STARTER;
+    private static final AbstractRelic.LandingSound LANDING_SOUND = AbstractRelic.LandingSound.MAGICAL;
+
+    public BiocircuitryRelic_Warforged() {
+
+    }
+}
