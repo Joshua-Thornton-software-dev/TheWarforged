976a338ecfcf79a82637f113e7715eda33de248e
Josh Thor <JoshuaThornton.SoftwareDev@Gmail.com>
2025-04-11 11:28:20 -0400

    Made Gyrospine code prettier

--- src/main/java/thewarforged/cards/powerCards/Gyrospine_Warforged.java
+++ src/main/java/thewarforged/cards/powerCards/Gyrospine_Warforged.java
@@ -13,9 +13,6 @@ import thewarforged.util.CardStats;
 public class Gyrospine_Warforged extends AbstractWarforgedCard {
     public static final String ID = makeID(Gyrospine_Warforged.class.getSimpleName());
 
-    private final int REGEN_ON_TRIGGER = 4;
-    private final int UPGRADED_REGEN_ON_TRIGGER_INCREASE = 0;
-
     private static final CardStats info = new CardStats(
             TheWarforged.Meta.CARD_COLOR,
             CardType.POWER,
@@ -23,6 +20,9 @@ public class Gyrospine_Warforged extends AbstractWarforgedCard {
             CardTarget.SELF,
             CardStats.POSITIVE_COST(2)
     );
+
+    private final int REGEN_ON_TRIGGER = 4;
+    private final int UPGRADED_REGEN_ON_TRIGGER_INCREASE = 0;
 
     public Gyrospine_Warforged() {
         super(ID, info);
@@ -33,14 +33,9 @@ public class Gyrospine_Warforged extends AbstractWarforgedCard {
 
     @Override
     public void use(AbstractPlayer player, AbstractMonster monster) {
-        for(AbstractPower pow : player.powers) {
-            if (pow.ID.equals(GyrospinePower_Warforged.POWER_ID)) {
-                return;
-            }
-        }
+        if (player.getPower(GyrospinePower_Warforged.POWER_ID) != null) return;
 
-        this.addToBot(
-                new ApplyPowerAction(
+        this.addToBot(new ApplyPowerAction(
                         player,
                         player,
                         new GyrospinePower_Warforged(player, this.magicNumber)));
