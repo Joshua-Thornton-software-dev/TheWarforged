ae7a8f7a2d0cbef126161eb58cda36bd12e470fe
Josh Thor <JoshuaThornton.SoftwareDev@Gmail.com>
2025-04-14 22:15:35 -0400

    Added Flux Node power card/buff
    FluxNode_Warforged is another Replacement Part card, a power that awards temporary Dexterity for playing cards with a high cost.
    
    This includes the card, the power (buff), the strings associated with each, and a getter on CrackedAetherheartRelic_Warforged to allow access to current energy gain per card.

---
+++ src/main/java/thewarforged/cards/powerCards/FluxNode_Warforged.java
@@ -0,0 +1,56 @@
+package thewarforged.cards.powerCards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import thewarforged.cards.AbstractWarforgedCard;
+import thewarforged.character.TheWarforged;
+import thewarforged.powers.FluxPower_Warforged;
+import thewarforged.util.CardStats;
+
+/**
+ * This class is for a power card that represents a missing/broken part for the Warforged, the Flux Node.
+ * Once played, any card played after with a cost of 4 or higher (variable) gains 3 (variable) temporary Dexterity.
+ * Upgraded, the threshold decreases by 1, and the temp Dex increases by 1.
+ * For the purposes of this effect, cards that cost X are considered to cost the player's total energy spent on
+ * the card, which would be their current total at the time the X-cost card is played + the amount of energy granted
+ * by the CrackedAetherheart for each card played, which varies.
+ */
+public class FluxNode_Warforged extends AbstractWarforgedCard {
+    public static final String ID = makeID(FluxNode_Warforged.class.getSimpleName());
+
+    private static final CardStats info = new CardStats(
+            TheWarforged.Meta.CARD_COLOR,
+            CardType.POWER,
+            CardRarity.UNCOMMON,
+            CardTarget.SELF,
+            CardStats.POSITIVE_COST(4)
+    );
+
+    //The amount of energy that a card must cost/use in order to trigger the effect.
+    private static final int ENERGY_COST_TO_ACTIVATE = 4;
+    private static final int UPGRADED_ENERGY_COST_TO_ACTIVATE_INCREASE = -1;
+
+    //The amount of temporary Dexterity that will be gained by this power when triggered.
+    private static final int DEX_GAINED = 3;
+    private static final int UPGRADED_DEX_GAINED_INCREASE = 1;
+    private static final String CUSTOM_VAR_NAME = "DEX_GAIN";
+
+    public FluxNode_Warforged() {
+        super(ID, info);
+        this.isReplacementCard = true;
+
+        setMagic(ENERGY_COST_TO_ACTIVATE, UPGRADED_ENERGY_COST_TO_ACTIVATE_INCREASE);
+        setCustomVar(CUSTOM_VAR_NAME, DEX_GAINED, UPGRADED_DEX_GAINED_INCREASE);
+    }
+
+    @Override
+    public void use(AbstractPlayer player, AbstractMonster monster) {
+        FluxPower_Warforged fluxPower = new FluxPower_Warforged(
+                player,
+                this.magicNumber,
+                this.customVar(CUSTOM_VAR_NAME)
+        );
+        this.addToBot(new ApplyPowerAction(player, player, fluxPower));
+    }
+}
---
+++ src/main/java/thewarforged/powers/FluxPower_Warforged.java
@@ -0,0 +1,78 @@
+package thewarforged.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.DexterityPower;
+import com.megacrit.cardcrawl.powers.LoseDexterityPower;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+import thewarforged.relics.starterRelics.CrackedAetherheartRelic_Warforged;
+import thewarforged.util.CardStats;
+
+import static thewarforged.TheWarforgedMod.makeID;
+
+public class FluxPower_Warforged extends AbstractWarforgedPower {
+    public static final String POWER_ID = makeID(FluxPower_Warforged.class.getSimpleName());
+    private static final AbstractPower.PowerType POWER_TYPE = PowerType.BUFF;
+    private static final boolean IS_TURNED_BASED = false;
+
+    private static CrackedAetherheartRelic_Warforged _aetherheartRelic;
+
+    /**
+     * Constructor.
+     * @param owner Must be the player, or this power will break
+     * @param energyToTrigger The amount of energy a card must cost/use in order to trigger the effect
+     * @param tempDexOnTrigger The amount of temporary Dexterity gained when triggered
+     */
+    public FluxPower_Warforged(AbstractCreature owner, int energyToTrigger, int tempDexOnTrigger) {
+        super(POWER_ID, POWER_TYPE, IS_TURNED_BASED, owner, energyToTrigger);
+        this.amount2 = tempDexOnTrigger;
+    }
+
+    private static CrackedAetherheartRelic_Warforged get_aetherheartRelic() {
+        if (_aetherheartRelic == null) {
+            AbstractRelic relic = AbstractDungeon.player.getRelic(CrackedAetherheartRelic_Warforged.ID);
+            _aetherheartRelic = (CrackedAetherheartRelic_Warforged) relic;
+        }
+        return _aetherheartRelic;
+    }
+
+    private static AbstractPlayer getPlayer() { return AbstractDungeon.player; }
+
+    @Override
+    public void updateDescription() {
+        this.description = this.DESCRIPTIONS[0]
+                + this.amount
+                + this.DESCRIPTIONS[1]
+                + this.amount2
+                + this.DESCRIPTIONS[2];
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action){
+        int cost = card.cost;
+        if (cost == CardStats.X_COST()) {
+            cost = EnergyPanel.getCurrentEnergy() + get_aetherheartRelic().getCurrentEnergyGain();
+        }
+        if (cost >= this.amount) {
+            AbstractPlayer player = getPlayer();
+
+            DexterityPower gainDexPower = new DexterityPower(player, this.amount2);
+            ApplyPowerAction positivePowerAction =
+                    new ApplyPowerAction(player, player, gainDexPower, this.amount2);
+            this.addToBot(positivePowerAction);
+
+            LoseDexterityPower loseDexterityPower = new LoseDexterityPower(player, this.amount2);
+            ApplyPowerAction negativePowerAction =
+                    new ApplyPowerAction(player, player, loseDexterityPower, this.amount2);
+            this.addToBot(negativePowerAction);
+
+            this.flash();
+        }
+    }
+}
